48ab1a767b5d67957c545e8b35d0c79225c0856a
NehaArshad <nehaarshad1203@gmail.com>
2025-01-31 00:45:25 +0500

    updateImageFieldKey

--- backend_code/Routes/shopRoutes.js
+++ backend_code/Routes/shopRoutes.js
@@ -4,10 +4,10 @@ import upload from '../MiddleWares/uploadimage.js';
 const {addshop,getallshops,getusershop,updateshop,getshopcategory,getshopName,deleteshopbyid} = shopControllers;
 const shopRouter = express.Router();
 
-shopRouter.post("/addshop/:id",upload.array('images',5),addshop);
+shopRouter.post("/addshop/:id",upload.array('image',5),addshop);
 shopRouter.get("/getallshops",getallshops);
 shopRouter.get("/getusershop/:id",getusershop);
-shopRouter.put("/updateshop/:id",upload.array('images',5),updateshop);
+shopRouter.put("/updateshop/:id",upload.array('image',5),updateshop);
 shopRouter.get("/getshopByName/:shopname",getshopName);
 shopRouter.get("/getshopByCategory/:category",getshopcategory);
 shopRouter.delete("/deleteshopbyid/:id",deleteshopbyid);
